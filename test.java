a0a6181045223b70bbfbf6ea9e563888755c44e9
Sabine Ingabire-test <ingabirezizas@gmail.com>
2026-02-26 18:09:04 +0200

    second commit

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         // TODO: Add test logic here
         System.out.println("This is a placeholder test.");
+        System.out.println("hello world");
     }
 }
